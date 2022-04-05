0f1da3ba2edd6f42c4f94362e7542245763b8522
nacmarino <nac.marino@gmail.com>
2022-04-04 22:42:47 -0300

    preparando para iniciar os trabalhos em cima do state of data 2021

---
+++ _posts/2022-12-31-state-of-data-2021/scripts/01_exploratoria.R
@@ -0,0 +1,9 @@
+
+# limpando o ambiente ---------------------------------------------------------------
+
+rm(list = ls(all.names = TRUE))
+
+# carregando pacotes ----------------------------------------------------------------
+
+library(tidyverse)
+
